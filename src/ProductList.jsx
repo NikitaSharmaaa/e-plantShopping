84f265fd02b1a93b188ad7688761e9bf05f7e2f1
nikita <nikitasharma16016@gmail.com>
2025-05-02 08:16:31 -0400

    final project

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -9,6 +9,8 @@ function ProductList({ onHomeClick }) {
     const [addedToCart, setAddedToCart] = useState({});
     const dispatch = useDispatch();
     const cart = useSelector(state => state.cart.items);
+    const totalQuantity = cart.reduce((acc, item) => acc + item.quantity, 0);
+
 
     const plantsArray = [
         {
@@ -283,7 +285,21 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div style={{ position: 'relative', display: 'inline-block' }}> 
+                    <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+                        <h1 className='cart'>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">
+                            <rect width="156" height="156" fill="none"></rect>
+                            <circle cx="80" cy="216" r="12"></circle>
+                            <circle cx="184" cy="216" r="12"></circle>
+                            <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>
+                        </svg>
+                        </h1>
+                    </a>
+                    {totalQuantity > 0 && (
+                        <span className="cart_quantity_count">{totalQuantity}</span>
+                    )}
+                    </div>
                 </div>
             </div>
             {!showCart ? (
@@ -303,7 +319,7 @@ function ProductList({ onHomeClick }) {
                             />
                             <div className="product-title">{plant.name}</div> {}
                             <div className="product-description">{plant.description}</div> {}
-                            <div className="product-cost">${plant.cost}</div> {}
+                            <div className="product-cost">{plant.cost}</div> {}
                             <button
                             className="product-button"
                             onClick={() => handleAddToCart(plant)}
